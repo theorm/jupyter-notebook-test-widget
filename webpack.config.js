86999c5c3b133edaa5caa5c0f54b7745d23e77c5
Roman Kalyakin <theorm@gmail.com>
2021-01-20 10:45:35 +0100

    Display source map (#2)
    * react integration
    
    * display source map

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const rules = [
       loader: 'babel-loader'
     }
   },
-  { test: /\.js$/, loader: 'source-map-loader' },
+  { test: /\.js$/, enforce: 'pre', loader: 'source-map-loader' },
   { test: /\.css$/, use: ['style-loader', 'css-loader']}
 ];
 
